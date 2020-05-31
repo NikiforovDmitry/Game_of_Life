c3b14e188c3b7197c703455e1b390f9064feec75
Dmitry Nikiforov <daimondhome@mail.ru>
2020-05-31 16:38:45 +0700

    Main function, InitializeWorld function

---
+++ src/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define _WORLD_WIDTH_ 20
+#define _WORLD_HEIGHT_ 20
+#define _SIZE_ 10
+
+struct point {
+    unsigned int Cell : 1;
+};
+struct cycleWorld {
+    struct point w[_WORLD_WIDTH_][_WORLD_HEIGHT_];
+};
+
+struct point world[_WORLD_WIDTH_][_WORLD_HEIGHT_];
+struct cycleWorld worlds[_SIZE_];
+
+unsigned int RandGen(int min, int max);
+void InitializeWorld(struct point world[_WORLD_WIDTH_][_WORLD_HEIGHT_]);
+
+unsigned int RandGen(int min, int max)
+{
+    return min + rand() % (max - min);
+}
+
+void InitializeWorld(struct point world[_WORLD_WIDTH_][_WORLD_HEIGHT_])
+{
+    int i, j, r = 0;
+    srand(time(NULL));
+    for (i = 1; i < _WORLD_WIDTH_; i++) {
+        for (j = 1; j < _WORLD_HEIGHT_; j++) {
+            r = RandGen(0, 1001);
+            if (r % 2 == 0) {
+                world[i][j].Cell = 0;
+            } else {
+                world[i][j].Cell = 1;
+            }
+        }
+    }
+}
+
+int main(int t, char const* n[])
+{
+    struct point world[_WORLD_WIDTH_][_WORLD_HEIGHT_];
+    struct point prev_world[_WORLD_WIDTH_][_WORLD_HEIGHT_];
+    InitializeWorld(world);
+}
