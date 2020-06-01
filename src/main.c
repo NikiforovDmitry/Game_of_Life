c2338e86857a847ca27c988cfdff394f5ef56be4
Dmitry Nikiforov <daimondhome@mail.ru>
2020-06-01 18:11:04 +0700

    Add function CoordinateNeighbours

--- src/main.c
+++ src/main.c
@@ -68,6 +68,21 @@ void CopyWorld(
     }
 }
 
+void CoordinateNeighbours(signed int nb[][2], unsigned int x, unsigned int y)
+{
+    unsigned int i, j, k = 0;
+    for (i = x - 1; i <= x + 1; i++) {
+        for (j = y - 1; j <= y + 1; j++) {
+            if (i == x && j == y) {
+                continue;
+            }
+            nb[k][0] = i;
+            nb[k][1] = j;
+            k++;
+        }
+    }
+}
+
 int main(int t, char const* n[])
 {
     struct point world[_WORLD_WIDTH_][_WORLD_HEIGHT_];
